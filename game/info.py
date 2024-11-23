ab21100419bc9762fdad81c10cc971fec760bbf6
Aiden <72966716+aidenbok203@users.noreply.github.com>
2024-11-23 13:04:42 +0000

    Create info.py

---
+++ game/info.py
@@ -0,0 +1,19 @@
+from classes import tile_definitions as t_def
+from classes import player_definitions as p_def
+
+tilesList = []
+
+# Load tiles.txt into classes in t_def
+def initialiseTiles():
+    with open("db/tiles.txt", "r") as f:
+        tiles = f.readlines()
+        for tile in tiles:
+            pos, name, cost, l0, l1, l2, l3 = tile.strip().split(",")
+            pos = t_def.Tile(int(pos), name, int(cost), int(l0), int(l1), int(l2), int(l3))
+            tilesList.append(pos)
+
+def intialisePlayers():
+    startMoney = int(input("Enter amount of money to start with: "))
+    for i in range(int(input("Input the number of players: "))):
+        name = input(f"Enter player {i + 1} name: ")
+        name = p_def.Player(i + 1, startMoney)
