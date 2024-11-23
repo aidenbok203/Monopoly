a6eabd7de0202d7f8206f3d261efaa0df10475bc
Aiden <72966716+aidenbok203@users.noreply.github.com>
2024-11-23 13:03:38 +0000

    Add files via upload

---
+++ classes/player_definitions.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, id, balance):
+        self.id = id
+        self.balance = balance
+        self.owned = []
---
+++ classes/tile_definitions.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class Tile:
+    pos: int
+    name: str
+    cost: int
+    l0: int
+    l1: int
+    l2: int
+    l3: int
+    level: int = 0
+    owned: bool = False
+
+    def __str__(self):
+        return f"Position: {self.pos}, Name: {self.name}, Cost: {self.cost}, L0: {self.l0}, L1: {self.l1}, L2: {self.l2}, L3: {self.l3}, Level {self.level}, Owned: {self.owned}"
