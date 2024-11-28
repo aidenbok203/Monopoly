c2c557b9e5706d7797e45c8d49da0e3899793a83
aidenbok203 <72966716+aidenbok203@users.noreply.github.com>
2024-11-28 08:44:34 +0000

    Fixed path logic

--- game/init.py
+++ game/init.py
@@ -16,7 +16,10 @@ def initialiseTiles() -> None:
     Initialise tile properties from database
     :return: None
     """
-    tiles_path = resource_path('../db/tiles.txt')
+    if getattr(sys, "frozen", False):
+        tiles_path = resource_path("../db/tiles.txt")
+    else:
+        tiles_path = resource_path("db/tiles.txt")
     with open(tiles_path, "r") as f:
         tiles = f.readlines()
         for tile in tiles:
